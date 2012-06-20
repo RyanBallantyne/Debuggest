5ee3160f3f5d9296e3e3366c35cf795adadb3c78
Ryan Ballantyne <ryan@i.tv>
2012-06-20 17:00:02 -0600

    Parser coalesces tokens for great justice.

--- Debuggest/ITVShellCommandParser.m
+++ Debuggest/ITVShellCommandParser.m
@@ -14,7 +14,86 @@
 - (void)parseAndRunCommandString:(NSString*)command
 {
     NSArray* tokens = [[[ITVShellCommandTokenizer alloc] init] tokenizeString:command];
-    NSLog(@"%@", tokens);
+    NSLog(@"%@", tokens);  //debug
+    tokens = [self coalesceTokens:tokens];
+    NSLog(@"%@", tokens);  //debug
+    
+//    ITVShellCommandToken* prevToken = nil;
+//    for (ITVShellCommandToken* token in tokens)  {
+//        if (!prevToken)  prevToken = token;
+//        else  {
+//            
+//        }
+//    }
+}
+
+- (NSArray*)coalesceTokens:(NSArray*)tokens
+{
+    NSMutableArray* coalescedTokens = [NSMutableArray arrayWithCapacity:[tokens count]];
+    NSMutableArray* tokensToCoalesce = [NSMutableArray arrayWithCapacity:10];
+    BOOL inQuotes = NO;
+    enum ITVShellCommandTokenType openQuoteType = -1;
+    
+    ITVShellCommandToken*(^coalesce)(NSArray*) = ^(NSArray* toCoalesce) {
+        NSMutableString* tokStr = [NSMutableString string];
+        
+        for (ITVShellCommandToken* tok in toCoalesce)  {
+            [tokStr appendString:tok.token];
+        }
+        
+        return [ITVShellCommandToken tokenWithTokenString:tokStr type:ITVStringToken];
+    };
+    
+    // Run through the array of tokens, adding things that might need coalescing to the tokensToCoalesce array.
+    // When we encounter something that should not be coalesced, we coalesce the tokensToCoalesce array.
+    // Everything inside quotes should be coalesced, and if the quoted sequence is adjacent to escape sequences or other strings, they should also be coalesced.
+    // Strings and escape sequences should be coalesced.
+    // Tildes should be replaced with the user's home directory, and coalesced into adjacent strings/escape sequences/quoted sequences
+    // Every other thing will trigger coalescense and empty out the toCoalesce array.
+    for (ITVShellCommandToken* tok in tokens)  {
+        switch (tok.type) {
+            case ITVSingleQuoteToken:
+            case ITVDoubleQuoteToken:  {
+                if (!inQuotes)  {
+                    inQuotes = YES;
+                    openQuoteType = tok.type;
+                }
+                else if (tok.type != openQuoteType)  {
+                    [tokensToCoalesce addObject:tok];
+                }
+                else  {
+                    inQuotes = NO;
+//                    [tokensToCoalesce addObject:coalesce(tokensToCoalesce)];
+                }
+                break;
+            }
+            case ITVStringToken:
+            case ITVEscapedCharacterToken:  {
+                [tokensToCoalesce addObject:tok];
+                break;
+            }
+            case ITVTildeToken:  {
+                [tokensToCoalesce addObject:[ITVShellCommandToken tokenWithTokenString:NSHomeDirectory() type:ITVStringToken]];
+                break;
+            }
+                
+            default:
+                if (inQuotes)  {
+                    [tokensToCoalesce addObject:tok];
+                    continue;
+                }
+                
+                if ([tokensToCoalesce count] > 0)  {
+                    [coalescedTokens addObject:coalesce(tokensToCoalesce)];
+                    [tokensToCoalesce removeAllObjects];
+                }                
+                [coalescedTokens addObject:tok];
+                break;
+        }
+    }
+    if ([tokensToCoalesce count] > 0)  [coalescedTokens addObject:coalesce(tokensToCoalesce)];
+    
+    return coalescedTokens;
 }
 
 @end
